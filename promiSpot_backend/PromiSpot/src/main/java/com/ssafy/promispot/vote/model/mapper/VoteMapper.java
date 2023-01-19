bf5e0fc748c4c1105a0948517d8eb0a18011fc58
최희경 <gmlrud9701@naver.com>
2023-01-19 17:39:40 +0900

    23.01.19_최희경_VoteMapper작성중

--- promiSpot_backend/PromiSpot/src/main/java/com/ssafy/promispot/vote/model/mapper/VoteMapper.java
+++ promiSpot_backend/PromiSpot/src/main/java/com/ssafy/promispot/vote/model/mapper/VoteMapper.java
@@ -13,22 +13,25 @@ import com.ssafy.promispot.vote.model.entity.VoteEntity;
 public interface VoteMapper {
  
 	//약속 장소 후보 등록
-	int insertCandidatePlace(VoteEntity voteEntity) throws SQLException;
+	public int insertCandidatePlace(VoteEntity voteEntity) throws SQLException;
 	
 	//약속 장소 후보 가져오기
-	VoteEntity getCandidatePlace(int voteId) throws SQLException;
+	public VoteEntity getCandidatePlace(int voteId) throws SQLException;
 	
 	//약속 장소 후보들 가져오기 - 하나의 약속에 속한 모든 장소 후보들
-	List<VoteEntity> getCandidatePlaceList(int promiseId) throws SQLException;
+	public List<VoteEntity> getCandidatePlaceList(int promiseId) throws SQLException;
 	
-	//약속 장소 후보 수정
-	int modifyCandidatePlace(int voteId) throws SQLException;
+	//약속 장소 후보 수정(투표/투표취소)
+	public int modifyCandidatePlace(int voteId) throws SQLException;
 	
 	//약속 장소 후보 삭제
-	int removeCandidatePlace(int voteId) throws SQLException;
+	public int removeCandidatePlace(int voteId) throws SQLException;
 	
 	//약속 장소 후보 랭크 가져오기 - 하나의 약속에 속한 모든 랭킹 계산후 가져옴
-	List<VoteEntity> getRankCandidatePlaceList(int promiseId) throws SQLException;
+	public List<VoteEntity> getRankCandidatePlaceList(int promiseId) throws SQLException;
+	
+	//사용자가 '한 약속에서 이 장소'(=약속장소투표번호 voteId로 구분)를 투표한 건지 아닌지 조회
+	public int isVoted(int promiseMemberId, int voteId) throws SQLException;
 	
 
 }
