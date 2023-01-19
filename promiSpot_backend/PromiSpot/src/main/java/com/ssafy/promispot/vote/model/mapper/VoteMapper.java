9f8692b3bdcf08d2a83c726b2814ea0886fbd1a6
최희경 <gmlrud9701@naver.com>
2023-01-19 15:27:44 +0900

    23.01.19_최희경_mapper수정

--- promiSpot_backend/PromiSpot/src/main/java/com/ssafy/promispot/vote/model/mapper/VoteMapper.java
+++ promiSpot_backend/PromiSpot/src/main/java/com/ssafy/promispot/vote/model/mapper/VoteMapper.java
@@ -1,5 +1,34 @@
 package com.ssafy.promispot.vote.model.mapper;
 
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.promispot.vote.model.entity.VoteEntity;
+
+
+@Mapper
 public interface VoteMapper {
+ 
+	//약속 장소 후보 등록
+	int insertCandidatePlace(VoteEntity voteEntity) throws SQLException;
 	
+	//약속 장소 후보 가져오기
+	VoteEntity getCandidatePlace(int voteId) throws SQLException;
+	
+	//약속 장소 후보들 가져오기 - 하나의 약속에 속한 모든 장소 후보들
+	List<VoteEntity> getCandidatePlaceList(int promiseId) throws SQLException;
+	
+	//약속 장소 후보 수정
+	int modifyCandidatePlace(int voteId) throws SQLException;
+	
+	//약속 장소 후보 삭제
+	int removeCandidatePlace(int voteId) throws SQLException;
+	
+	//약속 장소 후보 랭크 가져오기 - 하나의 약속에 속한 모든 랭킹 계산후 가져옴
+	List<VoteEntity> getRankCandidatePlaceList(int promiseId) throws SQLException;
+	
+
 }
