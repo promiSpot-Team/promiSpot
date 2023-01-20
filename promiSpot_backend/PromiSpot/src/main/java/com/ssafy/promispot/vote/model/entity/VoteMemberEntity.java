af0c92c0f1e1e4c1df00a61eef1edcc38834de70
최희경 <gmlrud9701@naver.com>
2023-01-20 15:48:10 +0900

    vote들 수정중

--- promiSpot_backend/PromiSpot/src/main/java/com/ssafy/promispot/vote/model/entity/VoteMemberEntity.java
+++ promiSpot_backend/PromiSpot/src/main/java/com/ssafy/promispot/vote/model/entity/VoteMemberEntity.java
@@ -8,52 +8,30 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel(value = "VoteMemberEntity : 투표자", description = "투표자를 나타내는 Entity입니다")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class VoteMemberEntity {
-	@ApiModelProperty(value = "투표자번호")
-	private int voteMemberId;
-	@ApiModelProperty(value = "약속번호")
-	private int promiseId;
-	@ApiModelProperty(value = "약속장소투표번호")
-	private int voteId;
-	@ApiModelProperty(value = "회원시퀀스")
+	@ApiModelProperty(value = "회원일련번호")
 	private int memberSeq;
-	@ApiModelProperty(value = "장소번호")
-	private String priceId;
+	@ApiModelProperty(value = "약속번호")
+	private int voteSeq;
 	
-	public int getVoteMemberId() {
-		return voteMemberId;
-	}
-	public void setVoteMemberId(int voteMemberId) {
-		this.voteMemberId = voteMemberId;
-	}
-	public int getPromiseId() {
-		return promiseId;
-	}
-	public void setPromiseId(int promiseId) {
-		this.promiseId = promiseId;
-	}
-	public int getVoteId() {
-		return voteId;
-	}
-	public void setVoteId(int voteId) {
-		this.voteId = voteId;
-	}
 	public int getMemberSeq() {
 		return memberSeq;
 	}
 	public void setMemberSeq(int memberSeq) {
 		this.memberSeq = memberSeq;
 	}
-	public String getPriceId() {
-		return priceId;
+	public int getVoteSeq() {
+		return voteSeq;
 	}
-	public void setPriceId(String priceId) {
-		this.priceId = priceId;
+	public void setVoteSeq(int voteSeq) {
+		this.voteSeq = voteSeq;
 	}
 	@Override
 	public String toString() {
-		return "VoteMemberEntity [voteMemberId=" + voteMemberId + ", promiseId=" + promiseId + ", voteId=" + voteId
-				+ ", memberSeq=" + memberSeq + ", priceId=" + priceId + "]";
+		return "VoteMemberEntity [memberSeq=" + memberSeq + ", voteSeq=" + voteSeq + "]";
 	}
+	
+	
+	
 	
 	
 	
