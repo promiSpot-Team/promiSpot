4d6c56ab9f7327061fb02208d0318eac3d002651
최희경 <gmlrud9701@naver.com>
2023-01-25 14:26:57 +0900

    23.01.25_최희경_버튼

--- promiSpot_frontend/promispot/src/App.js
+++ promiSpot_frontend/promispot/src/App.js
@@ -1,20 +1,17 @@
 import './App.css';
-import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
-import Start from './pages/Start'
+import { BrowserRouter, Route, Routes, useHistory } from 'react-router-dom';
+import Start from './pages/start'
 function App() {
   // const history = useHistory();
   return (
-    <SccContextProvider>
       <BrowserRouter>
-        <ScrollToTop />
-        <Switch>
-          <Route path="/start" exact component={Start} />
+        <Routes>
+          <Route path="/" element={<Start/>} />
           {/* <Route path="/form" component={Form} /> */}
           
           {/* <Route component={NotFound} /> */}
-        </Switch>
+        </Routes>
       </BrowserRouter>
-    </SccContextProvider>
   );
 }
 
--- promiSpot_frontend/promispot/src/pages/Start.js
+++ promiSpot_frontend/promispot/src/pages/Start.js
@@ -1,19 +1,22 @@
 import react, { useContext } from 'react';
 import { Route } from 'react-router-dom';
-import BagicButton from '../components/buttons/bagicButton'
+import BasicButton from '../components/buttons/basicButton'
 // import Header from '../components/header/header';
 // import Footer from '../components/footer/footer';
-// import './scss/main.scss';
+import './scss/start.scss';
 
 function Start() {
   return (
-    <div class="startWrapper">
+    <div className="startWrapper">
       {/* <Route component={Header} /> */}
-      <Route component={Logo} />
-      <Route component={BagicButton} />
-      <Route component={BagicButton} />
+      {/* <Route component={Logo} /> */}
+      {/* <Route component={BasicButton} /> */}
+      {/* <Route component={BasicButton} /> */}
+      <BasicButton text="로그인"
+      onClick={() => console.log('버튼 클릭 완료')}/>
+      <BasicButton text="회원가입"
+      onClick={() => console.log('버튼 클릭 완료@')}/>
       {/* <Route component={Footer} /> */}
-      
     </div>
   );
 }
