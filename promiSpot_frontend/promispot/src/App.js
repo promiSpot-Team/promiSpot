1c90e17d5e02fa24c869843ea6f16825813556c9
최희경 <gmlrud9701@naver.com>
2023-01-25 11:37:16 +0900

    react init

--- promiSpot_frontend/promispot/src/App.js
+++ promiSpot_frontend/promispot/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
+import Start from './pages/Start'
 function App() {
+  // const history = useHistory();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SccContextProvider>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Switch>
+          <Route path="/start" exact component={Start} />
+          {/* <Route path="/form" component={Form} /> */}
+          
+          {/* <Route component={NotFound} /> */}
+        </Switch>
+      </BrowserRouter>
+    </SccContextProvider>
   );
 }
 
---
+++ promiSpot_frontend/promispot/src/pages/Start.js
@@ -0,0 +1,21 @@
+import react, { useContext } from 'react';
+import { Route } from 'react-router-dom';
+import BagicButton from '../components/buttons/bagicButton'
+// import Header from '../components/header/header';
+// import Footer from '../components/footer/footer';
+// import './scss/main.scss';
+
+function Start() {
+  return (
+    <div class="startWrapper">
+      {/* <Route component={Header} /> */}
+      <Route component={Logo} />
+      <Route component={BagicButton} />
+      <Route component={BagicButton} />
+      {/* <Route component={Footer} /> */}
+      
+    </div>
+  );
+}
+
+export default Start;
