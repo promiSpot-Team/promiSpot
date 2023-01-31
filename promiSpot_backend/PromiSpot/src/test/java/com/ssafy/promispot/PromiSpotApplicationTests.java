12f5258218104418ae3e5bbad3e3bd734633458c
cladren123 <cladren12332@naver.com>
2023-01-31 13:40:02 +0900

    23.01.31_김동언_백엔드 수정2

--- promiSpot_backend/PromiSpot/src/test/java/com/ssafy/promispot/PromiSpotApplicationTests.java
+++ promiSpot_backend/PromiSpot/src/test/java/com/ssafy/promispot/PromiSpotApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ssafy.promispot;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PromispotApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.ssafy.promispot;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PromispotApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
