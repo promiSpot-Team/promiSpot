d67564b8c1c700c0b6b2067cf93dba47f0c95887
SuhyungK <dook4966@gmail.com>
2023-02-03 09:43:00 +0900

    23.02.03_김수형_지도검색수정

--- promiSpot_frontend/promispot/src/components/Search/SearchBar2.js
+++ promiSpot_frontend/promispot/src/components/Search/SearchBar2.js
@@ -11,6 +11,7 @@ const SearchBar2 = forwardRef(({ onClick, GetAxiosResponse, config }, ref) => {
 
   const onChange = (e) => {
     e.preventDefault();
+    setQuery(e.target.value)
     operation({
       ...config, 
       params: {
@@ -30,14 +31,12 @@ const SearchBar2 = forwardRef(({ onClick, GetAxiosResponse, config }, ref) => {
   }))
 
   function whileDragMapHandle() {
-    if (query !== '') {
-      operation({
-        ...config, 
-        params: {
-        ...config.params,
-        query}
-      });
-    }
+    operation({
+      ...config, 
+      params: {
+      ...config.params,
+      query}
+    });
   }
 
   return (
@@ -51,11 +50,11 @@ const SearchBar2 = forwardRef(({ onClick, GetAxiosResponse, config }, ref) => {
               id="outlined-basic"
               label="장소 입력"
               placeholder="검색어를 입력하세요"
-              
+              multiline
               variant="standard"
               fontFamily="Pretendard-Bold"
               margin="normal"
-              onKeyUp={onChange}
+              onChange={onChange}
             />
             {/* <BasicButton text="검색" /> */}
           </FormControl>
--- promiSpot_frontend/promispot/src/pages/PlaceMap/PlaceDetail.js
+++ promiSpot_frontend/promispot/src/pages/PlaceMap/PlaceDetail.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import store from '../../index'
+import { useNavigate, useLocation } from 'react-router-dom'
+import BasicHeader from '../../components/Header/BasicHeader1'
 
 export default function PlaceDetail(props) {
   const navigate = useNavigate()
+  const location = useLocation()
+  const place = location.state
 
   return (
     <div className="place-modal-wrapper">
-      <button onClick={() => navigate(-1)}>뒤로가기</button>
+      <BasicHeader text={place.place_name} />
+      <div>
+        <button style={{
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          backgroundColor: 'white',
+          border: '1px solid #c4c4c4',
+          marginRight: '1rem',
+          marginBottom: '1rem',
+          padding: '0.5rem 1rem',
+          borderRadius: '15px',
+        }}>
+          등록하기
+        </button>
+      </div>
     </div>
   )
 }
