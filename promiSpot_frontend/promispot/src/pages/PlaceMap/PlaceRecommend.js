bfdbfd82a8c9a3f61c437d430829fc3c86b77e70
yunjichoi9151 <cyjcode99@gmail.com>
2023-02-04 21:27:53 +0900

    23.02.04_최윤지_Map, TabBar 수정

--- promiSpot_frontend/promispot/src/pages/PlaceMap/PlaceRecommend.js
+++ promiSpot_frontend/promispot/src/pages/PlaceMap/PlaceRecommend.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import '../scss/Map_Container.scss'
-import { motion } from 'framer-motion'
+import React from "react";
+import BasicHeader4 from "../../components/Header/BasicHeader4";
+import "../scss/Map_Container.scss";
+import { motion } from "framer-motion";
 
 export default function PlaceRecommend() {
   return (
     <motion.div
-      className='place-modal-wrapper'
+      className="place-modal-wrapper"
       initial={{ opacity: 0, y: 15 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 15 }}
@@ -15,7 +16,7 @@ export default function PlaceRecommend() {
         // ease: [0, 0.71, 0.2, 1.01]
       }}
     >
-      <h2>장소 추천</h2>
+      <BasicHeader4 text="장소 추천" />
     </motion.div>
-  )
+  );
 }
--- promiSpot_frontend/promispot/src/pages/Promise/Promise.js
+++ promiSpot_frontend/promispot/src/pages/Promise/Promise.js
@@ -3,10 +3,10 @@ import BasicHeader2 from "../../components/Header/BasicHeader2";
 import PromiseList from "./PromiseList";
 import PromiseCalendar from "./PromiseCalendar";
 import TabBar from "../../components/TabBar/TabBar";
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
 import "../scss/Promise.scss";
 
 function TabPanel(props) {
@@ -38,12 +38,11 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
 export default function Promise(props) {
-
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
@@ -55,9 +54,9 @@ export default function Promise(props) {
       <div className="d-flex flex-column justify-content-center w-100 h-100">
         <div className="d-flex flex-column justify-content-center align-items-center"></div>
       </div>
-      <BasicHeader2 text="내 약속 보기"/>
+      <BasicHeader2 text="내 약속 보기" />
       <div className="friend-wrapper">
-      {/* <Link to = '/friend/list'>
+        {/* <Link to = '/friend/list'>
         <button onClick={() => setVisible1(!visible1)}>친구 리스트</button>
       </Link> 
       {visible1 && <FriendList/>}
@@ -72,22 +71,28 @@ export default function Promise(props) {
       <div className="friend-content-wrapper">
         {<FriendList/> */}
       </div>
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} textColor="secondary" indicatorColor="secondary" 
-          variant="fullWidth"
-          aria-label="full width tabs example" centered>
-          <Tab label="목록" {...a11yProps(0)} />
-          <Tab label="캘린더" {...a11yProps(1)} />
-        </Tabs>
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+            variant="fullWidth"
+            aria-label="full width tabs example"
+            centered
+          >
+            <Tab label="목록" {...a11yProps(0)} />
+            <Tab label="캘린더" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <PromiseList />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <PromiseCalendar />
+        </TabPanel>
       </Box>
-      <TabPanel value={value} index={0}>
-        <PromiseList/>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <PromiseCalendar/>
-      </TabPanel>
-    </Box>
       <TabBar />
     </>
   );
