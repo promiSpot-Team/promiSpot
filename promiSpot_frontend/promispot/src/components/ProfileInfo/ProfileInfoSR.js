8d30f82bd7a72fbec67c0a8446be506ba201ca95
yunji9151 <yunji9151@naver.com>
2023-02-06 15:04:57 +0900

    23.02.06_최윤지_수정

---
+++ promiSpot_frontend/promispot/src/components/ProfileInfo/ProfileInfoSR.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import MiniButton from '../Buttons/MiniButton';
+import './ProfileInfoSR.scss';
+
+export default function ProfileInfo(props) {
+
+  const {imgName, nickName, id, isValid} = props;
+  const imgUrl = "/images/" + imgName + ".jpg";
+
+  return (
+    <div className='profile-info-wrapper'>
+      <div className='profile-info-img-wrapper'>
+        <div className='profile-info-img'>
+        <img src={imgUrl} alt = {imgName} title = {imgName} width="40px"/></div>
+      </div>
+      <div className='profile-info-name-wrapper'>
+        <div className='profile-info-nickname-wrapper'>{nickName}</div>
+        <div className='profile-info-id-wrapper'>{id}</div>
+      </div>
+      <div className='profile-info-button-wrapper'>
+        <div className='profile-info-button'><button onClick={isValid}>요청</button></div>
+        
+      </div>
+    </div>
+  )
+}
--- promiSpot_frontend/promispot/src/pages/Friend/Friend.js
+++ promiSpot_frontend/promispot/src/pages/Friend/Friend.js
@@ -6,11 +6,13 @@ import FriendList from "./FriendList";
 import '../scss/Friend.scss';
 import FriendRequestReceive from "./FriendRequestReceive";
 import FriendRequestSend from "./FriendRequestSend";
+import FriendSearchList from "./FriendSearchList";
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import SearchBar from "../../components/Search/SearchBar";
 import Box from '@mui/material/Box';
+import {ImSearch} from "react-icons/im";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -49,6 +51,13 @@ export default function Friend(props) {
 
   const [value, setValue] = React.useState(0);
 
+  const [valid, setValid] = React.useState(false);
+
+  const isValid = () => {
+    console.log(valid);
+    setValid(!valid);
+  }
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
@@ -63,7 +72,13 @@ export default function Friend(props) {
     <BasicHeader2 text="친구 목록"/>
     <div className="friend-wrapper">
       <div className="friend-search-wrapper">
-      <SearchBar/></div>
+      
+    <div className="search">
+      <input type="text" className="search__input" placeholder="Search..." onClick={isValid}/>
+      <div className="search__icon">
+        <ImSearch/>
+      </div>
+    </div></div>
       {/* <Link to = '/friend/list'>
         <button onClick={() => setVisible1(!visible1)}>친구 리스트</button>
       </Link> 
@@ -79,24 +94,52 @@ export default function Friend(props) {
       <div className="friend-content-wrapper">
         {<FriendList/> */}
       </div>
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} textColor="secondary" indicatorColor="secondary" aria-label="basic tabs example" centered>
-          <Tab label="내 친구" {...a11yProps(0)} />
-          <Tab label="받은 요청" {...a11yProps(1)} />
-          <Tab label="보낸 요청" {...a11yProps(2)} />
-        </Tabs>
+      <div>
+        {
+          valid === false
+          ? <Box sx={{ width: '100%' }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={value} onChange={handleChange} textColor="secondary" indicatorColor="secondary" aria-label="basic tabs example" centered>
+              <Tab label="내 친구" {...a11yProps(0)} />
+              <Tab label="받은 요청" {...a11yProps(1)} />
+              <Tab label="보낸 요청" {...a11yProps(2)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
+            <FriendList/>
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <FriendRequestReceive/>
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            <FriendRequestSend/>
+          </TabPanel>
+        </Box>
+        : <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} textColor="secondary" indicatorColor="secondary" aria-label="basic tabs example" centered>
+            <Tab label="검색 결과" {...a11yProps(0)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+    <div className='profile-info-wrapper'>
+      <div className='profile-info-img-wrapper'>
+        <div className='profile-info-img'>
+        <img src={require("../../img/IU_Profile.jpg")} width="40px"/></div>
+      </div>
+      <div className='profile-info-name-wrapper'>
+        <div className='profile-info-nickname-wrapper'>닉네임</div>
+        <div className='profile-info-id-wrapper'>아이디</div>
+      </div>
+      <div className='profile-info-button-wrapper'>
+        <div className='profile-info-button'><button onClick={isValid}>요청</button></div>
+        
+      </div>
+    </div>
+        </TabPanel>
       </Box>
-      <TabPanel value={value} index={0}>
-        <FriendList/>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <FriendRequestReceive/>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <FriendRequestSend/>
-      </TabPanel>
-    </Box>
+        }
+    </div>
       <TabBar />
       </>
   );
