43980a38a58a2b9699f78252af683b4a7b5abec8
cladren123 <cladren12332@naver.com>
2023-02-06 09:48:45 +0900

    23.02.06_김동언_소켓 정립

--- promiSpot_backend/promispotback/src/main/java/com/ssafy/promispotback/websocket/controller/ChatRoomController.java
+++ promiSpot_backend/promispotback/src/main/java/com/ssafy/promispotback/websocket/controller/ChatRoomController.java
@@ -28,7 +28,8 @@ public class ChatRoomController {
     // 채팅 리스트 화면
     @GetMapping("/room")
     public String rooms(Model model) {
-        return "chat/room";
+        System.out.println("채팅방 들어와짐");
+        return "/chat/room";
     }
     
     // 모든 채팅방 목록 반환
