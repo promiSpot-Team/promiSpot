08d61932e30b042757d76e8176b0828aef0e7f2e
yunji9151 <yunji9151@naver.com>
2023-02-09 10:06:35 +0900

    23.02.09_최윤지_스케줄 페이지 수정

--- promiSpot_frontend/promispot/src/pages/PlaceMap/Schedule.js
+++ promiSpot_frontend/promispot/src/pages/PlaceMap/Schedule.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { BsFillCalendarCheckFill } from "react-icons/bs";
 import '../scss/Map_Container.scss'
 
 export default function Schedule() {
 
 return (
-  <div className='schedule-background-wrapper'></div>
+  <div className='schedule-background-wrapper'>
+    <div className='map-button-wrapper'>
+      <button className="map-button-schedule">
+        <BsFillCalendarCheckFill size="40" color="#ffffff" />
+      </button>
+    </div>
+  </div>
 );
 }
