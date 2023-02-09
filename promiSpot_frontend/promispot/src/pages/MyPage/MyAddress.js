dca458375f9e010f854871000ba3ecd8e02273e5
yunjichoi9151 <cyjcode99@gmail.com>
2023-02-10 00:15:00 +0900

    23.02.10_최윤지_일부 수정

--- promiSpot_frontend/promispot/src/pages/MyPage/MyAddress.js
+++ promiSpot_frontend/promispot/src/pages/MyPage/MyAddress.js
@@ -3,84 +3,83 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import TabBar from "../../components/TabBar/TabBar";
 import { SERVER_URL } from "../../constants/constants";
-import ProfileInfoS from '../../components/ProfileInfo/ProfileInfoS'
-import { useSelector, useDispatch } from 'react-redux'
-import { reissueToken } from '../../reducer/user'
-import '../scss/MyPage.scss'
+import ProfileInfoS from "../../components/ProfileInfo/ProfileInfoS";
+import { useSelector, useDispatch } from "react-redux";
+import { reissueToken } from "../../reducer/user";
+import "../scss/MyPage.scss";
 
 export default function MyAddress() {
   const [myAddressList, setMyAddressList] = useState([]);
-  const [axiosMethod, setAxiosMethod] = useState('')
-  const [addressNum, setAddressNum] = useState('')
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const {
-    memberSeq,
-    addressSeq
-  } = useSelector(state => state.user.info)
+  const [axiosMethod, setAxiosMethod] = useState("");
+  const [addressNum, setAddressNum] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { memberSeq, addressSeq } = useSelector((state) => state.user.info);
 
   const getMyAddress = async () => {
     try {
       const response1 = await axios({
-        method: 'GET',
+        method: "GET",
         url: `${SERVER_URL}/address/addressList/${memberSeq}`,
-      })
-      if(response1.data !== 'fail') {
-        console.log("is in?")
-        setMyAddressList(response1.data)
+      });
+      if (response1.data !== "fail") {
+        console.log("is in?");
+        setMyAddressList(response1.data);
       }
-    } catch(err) {
-      console.log(err)
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
   const deleteMyAddress = async () => {
     try {
       const response2 = await axios({
         method: axiosMethod,
         url: `${SERVER_URL}/address/${addressSeq}`,
-      })
-    } catch(err) {
-      console.log(err)
+      });
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
   useEffect(() => {
-    getMyAddress()
-  }, [])
+    getMyAddress();
+  }, []);
+
+  useEffect(() => {}, [myAddressList]);
 
   useEffect(() => {
-
-  }, [myAddressList])
-
-  useEffect(() => {
-    if(axiosMethod === 'DELETE') {
-      deleteMyAddress()
+    if (axiosMethod === "DELETE") {
+      deleteMyAddress();
     }
-  }, [axiosMethod])
+  }, [axiosMethod]);
 
-
-  const onClick = (method, num) => {
-    setAxiosMethod(method)
-    setAddressNum(num)
-  }
+  const onClick = (method) => {
+    setAxiosMethod(method);
+  };
 
   function goAddress() {
-    window.location.replace('/address/search');
+    window.location.replace("/address/search");
   }
 
   return (
     <div>
-      {myAddressList && myAddressList.map((item, idx) => {
-        return (
-          <div key={idx}>
-            {item.addressAdress}
-            <button onClick={() => {onClick('DELETE', item.addressSeq); }}>삭제</button>
-            <button>기본 주소로 설정</button>
-          </div>
-        )
-      })}
-      
+      {myAddressList &&
+        myAddressList.map((item, idx) => {
+          return (
+            <div key={idx}>
+              {item.addressAdress}
+              <button
+                onClick={() => {
+                  onClick("DELETE");
+                }}
+              >
+                삭제
+              </button>
+              <button>기본 주소로 설정</button>
+            </div>
+          );
+        })}
     </div>
-  )
+  );
 }
