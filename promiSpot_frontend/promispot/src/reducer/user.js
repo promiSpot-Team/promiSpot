79e672852e69e2cf17f37bb18a34660d8c110cca
yunji9151 <yunji9151@naver.com>
2023-02-10 17:35:53 +0900

    23.02.10_최윤지_마이페이지 수정

--- promiSpot_frontend/promispot/src/reducer/user.js
+++ promiSpot_frontend/promispot/src/reducer/user.js
@@ -1,60 +1,75 @@
 /* 액션 타입 만들기 */
-const SET_TOKEN = 'user/SET_TOKEN';
-const REISSUE_TOKEN = 'user/REFRESH_TOKEN';
-const SET_JOIN_INFO = 'user/SET_JOIN_INFO'
-const SET_ADDRESS = 'user/SET_ADDRESS'
+const SET_TOKEN = "user/SET_TOKEN";
+const REISSUE_TOKEN = "user/REFRESH_TOKEN";
+const SET_JOIN_INFO = "user/SET_JOIN_INFO";
+const SET_ADDRESS = "user/SET_ADDRESS";
+// const EDIT_INFO = "user/EDIT_INFO";
 
 /* 액션 생성함수 만들기 */
-export const setToken = info => ({ type: SET_TOKEN, info })
-export const reissueToken = accessToken => ({ type: REISSUE_TOKEN, accessToken })
-export const setJoinInfo = joinInfo => ({ type: SET_JOIN_INFO, joinInfo })
-export const setAddress = addressInfo => ({ type: SET_ADDRESS, addressInfo })
+export const setToken = (info) => ({ type: SET_TOKEN, info });
+export const reissueToken = (accessToken) => ({
+  type: REISSUE_TOKEN,
+  accessToken,
+});
+export const setJoinInfo = (joinInfo) => ({ type: SET_JOIN_INFO, joinInfo });
+export const setAddress = (addressInfo) => ({ type: SET_ADDRESS, addressInfo });
+// export const editInfo = (myinfo) => ({ type: SET_TOKEN, myinfo });
 
 /* 초기값 설정 */
 const initialState = {
   isLogin: false,
   joinInfo: {
-    id: '',
-    email: '', 
-    password:'',
-    name: '',
-    nickName:'', 
-    phoneNumber: ''
+    id: "",
+    email: "",
+    password: "",
+    name: "",
+    nickName: "",
+    phoneNumber: "",
   },
-}
+};
 
 /* 리듀서 선언 */
 export default function user(state = initialState, action) {
   switch (action.type) {
-    /* 로그인 성공하면 토큰 저장 */ 
+    /* 로그인 성공하면 토큰 저장 */
     case SET_TOKEN: {
       return Object.assign({}, state, {
-        info: action.info
-      })
+        info: action.info,
+      });
     }
-    
+
     /* 토큰 재발급 */
     case REISSUE_TOKEN: {
-      const newUserInfo = {...state.info, accessToken: action.accessToken}
-      console.log(newUserInfo)
+      const newUserInfo = { ...state.info, accessToken: action.accessToken };
+      console.log(newUserInfo);
       return Object.assign({}, state, {
-        ...state, info: {...newUserInfo}
-      })
+        ...state,
+        info: { ...newUserInfo },
+      });
     }
 
     /* 회원가입 임시 정보 저장 */
     case SET_JOIN_INFO: {
       return Object.assign({}, state, {
-        joinInfo: action.joinInfo
-      })
+        joinInfo: action.joinInfo,
+      });
     }
 
     /* 주소 정보 저장 */
     case SET_ADDRESS: {
       return Object.assign({}, state, {
-        addressInfo: action.addressInfo
-      })
+        addressInfo: action.addressInfo,
+      });
     }
+
+    // case EDIT_INFO: {
+    //   const editUserInfo = { ...state.info, memberNick: action.memberNick };
+    //   console.log(editUserInfo);
+    //   return Object.assign({}, state, {
+    //     ...state,
+    //     info: { ...editUserInfo },
+    //   });
+    // }
     default:
       return state;
   }
