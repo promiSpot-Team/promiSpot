62673079adc2b4b03ecdd293a9112a91c03181d3
SuhyungK <dook4966@gmail.com>
2023-02-16 10:42:17 +0900

    23.02.16_김수형_수정 끝

--- promiSpot_frontend/promispot/src/Route/NotFound.js
+++ promiSpot_frontend/promispot/src/Route/NotFound.js
@@ -1,7 +1,21 @@
+import { Navigate } from 'react-router-dom'
+import React from 'react'
 
 export default function NotFound() {
+
+  // const moveToMain = () => {
+  //   setTimeout(() => {
+  //     console.log('??')
+  //     Navigate('/main')
+  //   }, 3000)
+  // }
+
+  // React.useEffect(() => {
+  //   moveToMain()
+  // }, [])
+
   return (
-    <div>
+    <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center', color: '#c4c4c4',}}>
       <h1>PAGE NOT FOUND</h1>
     </div>
   )
