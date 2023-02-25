57545cff14ffcc20b6a039a22904a0704393fcee
SuhyungK <dook4966@gmail.com>
2023-02-25 11:56:28 +0900

    23.02.25_김수형_키 삭제

--- promiSpot_frontend/promispot/src/constants/constants.js
+++ promiSpot_frontend/promispot/src/constants/constants.js
@@ -1,6 +1,6 @@
 // BACKEND API
-export const SERVER_URL = 'https://i8a109.p.ssafy.io/api'
+export const SERVER_URL = 'http://localhost:9090'
 
 // KAKAO MAP API
-export const KAKAO_MAP_URL = 'https://dapi.kakao.com'
-export const KAKAO_REST_API_KEY = '64bbe2063e9e0b314a6060be44144a26'
+export const KAKAO_MAP_URL = ''
+export const KAKAO_REST_API_KEY = ''
